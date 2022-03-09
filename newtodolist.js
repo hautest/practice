48e4bd2d836e43a0c09a0e2d73eb81bab2a3af85
hautest <dos07008@naver.com>
2022-03-09 14:12:10 +0900

    todolistdd

---
+++ newtodolist.js
@@ -0,0 +1,74 @@
+//변수선언
+const main = document.querySelector(".main");
+const button = document.querySelector(".button");
+const list = document.querySelector(".list")
+const input =document.querySelector("#input");
+const form =document.querySelector("#form");
+let savelist = JSON.parse( localStorage.getItem("todosave"));
+let todosave = [];
+
+    //빈값 입력 방지
+    form.addEventListener('submit', A)
+    function A(a) {
+        a.preventDefault();
+    if (input.value ===""){
+        alert("리스트를 입력해주십시오");
+     } else makelist()
+    }
+
+
+//입력 받아서 리스트 만드는 함수
+function makelist() {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const delete1 = document.createElement("button");
+  li.appendChild(span);
+  span.innerText = (input.value || savelist[i].text);
+  list.appendChild(li);
+  li.appendChild(delete1);
+  delete1.innerText = "X"
+
+//삭제버튼 누르면 삭제되는 함수
+delete1.addEventListener('click', Fdelete)
+    function Fdelete(event) {
+    let li2 = event.target.parentElement;
+    let deletedList = li2.id
+    console.log(deletedList);
+    list.removeChild(li);
+    li.removeChild(span);
+    todosave = todosave.filter((toDo) => toDo.id !== parseInt(li2.id));
+    console.log(todosave);
+    localStorage.setItem("todosave",JSON.stringify(todosave) ); 
+    }
+//입력값을 로컬스로지에 저장
+    const todoobject = {
+      text: input.value ||  savelist[i].text ,
+      id: Math.floor(Math.random()*1000000)+1,
+  }
+  li.id = todoobject.id;
+  todosave.push(todoobject || savelist[i]);
+  localStorage.setItem("todosave",JSON.stringify(todosave) ); 
+  input.value = '';
+
+  //클릭하면 선 생기는 함수
+span.addEventListener("click", check);
+function check () { 
+     span.classList.toggle("checkText")
+     
+}
+
+
+
+
+}
+
+//새로고침하면 데이터 유지시키는 함수
+if (location.reload && localStorage.length !== 0) {
+    console.log("reload");
+    for (i=0; savelist[i]; i++){
+
+
+    makelist()
+}}
+
+
--- todolist.js
+++ todolist.js
@@ -3,6 +3,8 @@ const button = document.querySelector(".button");
 const list = document.querySelector(".list")
 const input =document.querySelector("#input");
 const form =document.querySelector("#form");
+const span =document.querySelector("span")
+const todosave = [];
 
 function A(event) { 
     event.preventDefault();
@@ -19,23 +21,81 @@ else {
   list.appendChild(li);
   li.appendChild(delete1);
   delete1.innerText = "X"
+
+  todosave.push(input.value);
+  localStorage.setItem("todosave",JSON.stringify(todosave) ); 
   input.value = '';
- 
-
-
+  
 
   //삭제버튼 클릭
   delete1.onclick = function B () { 
+    todosave.splice(0, todosave.length);
+    localStorage.clear(); 
+    todosave.push(span.value);  
+    localStorage.setItem("todosave",JSON.stringify(todosave) ); 
     list.removeChild(li);
-    li.removeChild(span);
-      
+    li.removeChild(span); 
+      }
+
+
+
+
+ //리스트에 써놓은것 클릭하면 선 그어지기
+
+span.addEventListener("click", check);
+
+function check () { 
+     span.classList.toggle("checkText")
 }
 }
 
 } 
-form.addEventListener('submit', A)
-
-
-
-
+//새로고침하면 값 유지하기
+if (location.reload && localStorage.length !== 0) {
+    console.log("reload");
+    let savelist = JSON.parse( localStorage.getItem("todosave"));
   
+   
+    for (i=0; savelist[i]; i++){
+        const li = document.createElement("li");
+        list.appendChild(li);
+        const span = document.createElement("span");
+        li.appendChild(span);
+        span.innerText = savelist[i]
+        const delete1 = document.createElement("button");
+        li.appendChild(delete1);
+        delete1.innerText = "X"
+
+        delete1.onclick = function B () { 
+            todosave.splice(0, todosave.length);
+            localStorage.clear(); 
+            todosave.push(span.value);  
+            localStorage.setItem("todosave",JSON.stringify(todosave) ); 
+            list.removeChild(li);
+            li.removeChild(span); 
+              }
+
+              span.addEventListener("click", check);
+
+            function check () { 
+                span.classList.toggle("checkText")
+}
+
+
+    }
+
+
+     
+    
+        
+    
+    
+
+
+ 
+}
+
+ 
+
+
+form.addEventListener('submit', A)
