1bab99e7952b8f41f5c5a4f01b59fb1707b86772
lee <lee@sayongjaui-MacBookAir.local>
2022-04-20 17:56:25 +0900

    redux-study-banilla-js-redux-no

--- studyEx/vanilla Redux/vanillaredux/src/index.js
+++ studyEx/vanilla Redux/vanillaredux/src/index.js
@@ -3,6 +3,7 @@ const minus = document.querySelector(".minus");
 const number = document.querySelector("span");
 
 let count = 0;
+let a = 0;
 
 const updateText = () => {
   number.innerText = count;
